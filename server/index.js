16d8991a55c060f018635d4154f3a04d7a0a3f4e
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-05-22 14:02:10 -0700

    added vercel.json and updated urls for deployment

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://daisy-ui-mern-auth-frontend.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 const dbConnect = async () => {
