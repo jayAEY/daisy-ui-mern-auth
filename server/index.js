3f5df2aecadf04f4133736863f6404c7b76f5619
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-05-22 14:16:20 -0700

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://daisy-ui-mern-auth-frontend.vercel.app/",
+      "https://daisy-ui-mern-auth-frontend.vercel.app",
     ],
     credentials: true,
   })
